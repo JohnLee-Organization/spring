d951cf116b203c354d77a4a6b7e66009a2483a37
lz958942 <404644381@qq.com>
2018-05-28 15:48:34 +0800

    增加mybatis的redis缓存功能

--- cache/mybatis/src/main/java/net/lizhaoweb/spring/cache/mybatis/RedisCache.java
+++ cache/mybatis/src/main/java/net/lizhaoweb/spring/cache/mybatis/RedisCache.java
@@ -22,6 +22,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.util.List;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 使用第三方内存数据库Redis作为二级缓存
@@ -44,7 +45,7 @@ public class RedisCache implements Cache {
     private final String id;
 
     @Getter
-    private final ReadWriteLock readWriteLock = new DummyReadWriteLock(); // new ReentrantReadWriteLock();
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     @Setter
     private static JedisConnectionFactory jedisConnectionFactory;
