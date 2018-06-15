56d470d4722ead7e4d6b9ff058fe5861de3068b5
lz958942 <404644381@qq.com>
2018-06-15 17:57:44 +0800

    修改

---
+++ mvc/core/src/main/java/net/lizhaoweb/spring/mvc/core/annotation/APIVersion.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2018, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @Project : spring
+ * @Package : net.lizhaoweb.spring.mvc.core.annotation
+ * @author <a href="http://www.lizhaoweb.net">李召(John.Lee)</a>
+ * @EMAIL 404644381@qq.com
+ * @Time : 15:56
+ */
+package net.lizhaoweb.spring.mvc.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * <h1>注解 - API 接口版本</h1>
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 1.0.0.0.1
+ * @EMAIL 404644381@qq.com
+ * @notes Created on 2018年05月30日<br>
+ * Revision of last commit:$Revision$<br>
+ * Author of last commit:$Author$<br>
+ * Date of last commit:$Date$<br>
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface APIVersion {
+}
