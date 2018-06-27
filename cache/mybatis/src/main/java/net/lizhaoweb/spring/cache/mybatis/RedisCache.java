625a2cfa6f6a116ed14744d11ddf56527a3040dd
lz958942 <404644381@qq.com>
2018-06-27 13:56:19 +0800

    修改

--- cache/mybatis/src/main/java/net/lizhaoweb/spring/cache/mybatis/RedisCache.java
+++ cache/mybatis/src/main/java/net/lizhaoweb/spring/cache/mybatis/RedisCache.java
@@ -47,22 +47,19 @@ public class RedisCache implements Cache {
     @Getter
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
+    /**
+     * 需要静态注入，这里通过中间类 MybatisRedisCacheTransfer 实现的
+     */
     @Setter
     private static JedisConnectionFactory jedisConnectionFactory;
 
     public RedisCache(String id) {
-        if (null == id || "".equals(id.trim())) {
+        if (null == id || id.trim().length() < 1) {
             throw new IllegalArgumentException("Mybatis redis cache need an id.");
         }
         this.id = id;
         LOGGER.debug("Mybatis redis cache id: {}", id);
     }
-
-
-//    @Override
-//    public String getId() {
-//        return this.id;
-//    }
 
     /**
      * 存值
@@ -76,23 +73,6 @@ public class RedisCache implements Cache {
             return;
         }
         LOGGER.debug("Mybatis redis cache put. RedisKey={} RedisValue={}", key, value);
-//        RedisConnection redisConnection = null;
-//        try {
-//            redisConnection = jedisConnectionFactory.getConnection();
-//            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer();
-//            redisConnection.set(serializer.serialize(key), serializer.serialize(value));
-//
-//            // 将key保存到redis.list中
-//            redisConnection.lPush(serializer.serialize(id), serializer.serialize(key));
-//        } catch (Exception e) {
-//            LOGGER.error("Mybatis redis cache put exception. RedisKey=" + key + " RedisValue=" + value, e);
-//        } finally {
-////            if (null != redisConnection) {
-////                redisConnection.close();
-////            }
-//            this.close(redisConnection);
-//        }
-
         this.execute(new RedisCallback<Void>() {
             @Override
             public Void doWithRedis(RedisConnection connection) {
@@ -117,23 +97,6 @@ public class RedisCache implements Cache {
             return null;
         }
         LOGGER.debug("Mybatis redis cache get. RedisKey={}", key);
-//        RedisConnection redisConnection = null;
-//        Object result = null;
-//        try {
-//            redisConnection = jedisConnectionFactory.getConnection();
-//            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer();
-//            result = serializer.deserialize(redisConnection.get(serializer.serialize(key)));
-//        } catch (Exception e) {
-//            LOGGER.error("Mybatis redis cache get exception. RedisKey=" + key + " RedisValue=null", e);
-//        } finally {
-////            if (null != redisConnection) {
-////                redisConnection.close();
-////            }
-//            this.close(redisConnection);
-//        }
-//        return result;
-
-
         return this.execute(new RedisCallback<Object>() {
             @Override
             public Object doWithRedis(RedisConnection connection) {
@@ -154,27 +117,6 @@ public class RedisCache implements Cache {
             return null;
         }
         LOGGER.debug("Mybatis redis cache remove. RedisKey={}", key);
-//        RedisConnection redisConnection = null;
-//        Object result = null;
-//        try {
-//            redisConnection = jedisConnectionFactory.getConnection();
-//            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer();
-//            // 讲key设置为立即过期
-//            result = redisConnection.expireAt(serializer.serialize(key), 0);
-//
-//            // 将key从redis.list中删除
-//            redisConnection.lRem(serializer.serialize(id), 0, serializer.serialize(key));
-//        } catch (Exception e) {
-//            LOGGER.error("Mybatis redis cache remove exception. RedisKey=" + key + " RedisValue=" + result, e);
-//        } finally {
-////            if (null != redisConnection) {
-////                redisConnection.close();
-////            }
-//            this.close(redisConnection);
-//        }
-//        return result;
-
-
         return this.execute(new RedisCallback<Object>() {
             @Override
             public Object doWithRedis(RedisConnection connection) {
@@ -195,35 +137,6 @@ public class RedisCache implements Cache {
     @Override
     public void clear() {
         LOGGER.debug("Mybatis redis cache clear.");
-//        RedisConnection redisConnection = null;
-//        try {
-//            redisConnection = jedisConnectionFactory.getConnection();
-//            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer();
-//
-//            /*
-//             * 千万不要直接使用 redisConnection.scriptFlush()、redisConnection.flushDb() 或 redisConnection.flushAll()，
-//             * 会把整个redis的东西都清除掉，我不相信你的redis里没有其他东西。获取redis.list中的保存的key值，遍历删除
-//             */
-//            Long length = redisConnection.lLen(serializer.serialize(id));
-//            if (0 == length) {
-//                return;
-//            }
-//            List<byte[]> keyList = redisConnection.lRange(serializer.serialize(id), 0, length - 1);
-//            for (byte[] key : keyList) {
-//                redisConnection.expireAt(key, 0);
-//            }
-//            redisConnection.expireAt(serializer.serialize(id), 0);
-//        keyList.clear();
-//    } catch (Exception e) {
-//        LOGGER.error("Mybatis redis cache clear exception.", e);
-//    } finally {
-////            if (null != redisConnection) {
-////                redisConnection.close();
-////            }
-//        this.close(redisConnection);
-//    }
-
-
         this.execute(new RedisCallback<Void>() {
             @Override
             public Void doWithRedis(RedisConnection connection) {
@@ -247,18 +160,6 @@ public class RedisCache implements Cache {
 
     @Override
     public int getSize() {
-//        int result = 0;
-//        try {
-//            RedisConnection redisConnection = jedisConnectionFactory.getConnection();
-//            long dbSize = redisConnection.dbSize();
-//            if ((int) dbSize != dbSize) {
-//                throw new ArithmeticException("integer overflow");
-//            }
-//            result = (int) dbSize;
-//        } catch (Exception e) {
-//            LOGGER.error("Mybatis redis cache getSize exception. ", e);
-//        }
-
         return this.execute(new RedisCallback<Integer>() {
             @Override
             public Integer doWithRedis(RedisConnection connection) {
@@ -273,21 +174,8 @@ public class RedisCache implements Cache {
         });
     }
 
-//    @Override
-//    public ReadWriteLock getReadWriteLock() {
-//        return this.readWriteLock;
-//    }
 
-//    /**
-//     * 这个地方需要静态注入，这里通过中间类 MybatisRedisCacheTransfer 实现的
-//     *
-//     * @param jedisConnectionFactory 连接工厂创建基于 JEDIS 的连接。
-//     */
-//    public static void setJedisConnectionFactory(JedisConnectionFactory jedisConnectionFactory) {
-//        RedisCache.jedisConnectionFactory = jedisConnectionFactory;
-//    }
-
-    // 执行回调
+    // 执行 Redis 回调
     private <T> T execute(RedisCallback<T> callback) {
         RedisConnection redisConnection = null;
         try {
