5280ed59b95c35e7383c39dbeb6e8150039ed2b5
lz958942 <404644381@qq.com>
2019-08-14 11:49:04 +0800

    修改

---
+++ file/upload-download-api/src/test/java/net/lizhaoweb/spring/file/TestFileEngine.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2019, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @Project : spring
+ * @Package : net.lizhaoweb.spring.file
+ * @author <a href="http://www.lizhaoweb.net">李召(John.Lee)</a>
+ * @EMAIL 404644381@qq.com
+ * @Time : 11:43
+ */
+package net.lizhaoweb.spring.file;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 1.0.0.0.1
+ * @EMAIL 404644381@qq.com
+ * @notes Created on 2019年08月14日<br>
+ * Revision of last commit:$Revision$<br>
+ * Author of last commit:$Author$<br>
+ * Date of last commit:$Date$<br>
+ */
+public class TestFileEngine {
+    private IEngine engine;
+
+    @Before
+    public void init() {
+        FileEngine fileEngine = new FileEngine();
+        this.engine = fileEngine;
+    }
+
+    @Test
+    public void start() {
+        this.engine.init();
+        this.engine.start();
+    }
+}
---
+++ file/upload-download-oss/src/main/java/net/lizhaoweb/spring/file/OSSFileTransferExecutor.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2019, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @Project : spring
+ * @Package : net.lizhaoweb.spring.file
+ * @author <a href="http://www.lizhaoweb.net">李召(John.Lee)</a>
+ * @EMAIL 404644381@qq.com
+ * @Time : 11:38
+ */
+package net.lizhaoweb.spring.file;
+
+/**
+ * <h1>执行器 [实现] - OSS文件传输</h1>
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 1.0.0.0.1
+ * @EMAIL 404644381@qq.com
+ * @notes Created on 2019年08月14日<br>
+ * Revision of last commit:$Revision$<br>
+ * Author of last commit:$Author$<br>
+ * Date of last commit:$Date$<br>
+ */
+public class OSSFileTransferExecutor implements IExecutor<FileContext> {
+
+    @Override
+    public boolean execute(FileContext context) {
+        return false;
+    }
+}
