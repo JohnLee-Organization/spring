b32bb5f04e6bca8e6ee180dc42014366e77fc741
lz958942 <404644381@qq.com>
2023-08-03 16:53:25 +0800

    修改

---
+++ mvc/licenses/truelicense/src/main/java/net/lizhaoweb/interceptor/LicenseInterceptor.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ * @project : spring
+ * @package : net.lizhaoweb.lic.truelicense
+ * @date : 2023-08-03
+ * @time : 15:58
+ */
+package net.lizhaoweb.interceptor;
+
+import com.alibaba.fastjson2.JSONObject;
+import net.lizhaoweb.lic.truelicense.c.LicenseVerify;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器校验license证书
+ * <p>
+ * Created by Jhon.Lee on 8/3/2023 3:58 PM
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 0.0.1
+ * @email 404644381@qq.com
+ */
+@Component
+public class LicenseInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        LicenseVerify licenseVerify = new LicenseVerify();
+
+        //校验证书是否有效
+        boolean verifyResult = licenseVerify.verify();
+
+        if (verifyResult) {
+            return true;
+        } else {
+            response.setCharacterEncoding("utf-8");
+            JSONObject obj = new JSONObject();
+            obj.put("errcode", "0319");
+            obj.put("errmsg", "您的证书无效，请核查服务器是否取得授权或重新申请证书！");
+            response.getWriter().print(obj);
+            response.getWriter().flush();
+            return false;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+
+}
---
+++ mvc/licenses/truelicense/src/main/java/net/lizhaoweb/listener/LicenseCheckListener.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2023, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ * @project : spring
+ * @package : net.lizhaoweb.lic.truelicense
+ * @date : 2023-08-03
+ * @time : 15:53
+ */
+package net.lizhaoweb.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import net.lizhaoweb.lic.truelicense.c.LicenseVerify;
+import net.lizhaoweb.lic.truelicense.vo.LicenseVerifyParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * 在项目启动的时候安装证书的Listener类
+ * <p>
+ * Created by Jhon.Lee on 8/3/2023 3:53 PM
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 0.0.1
+ * @email 404644381@qq.com
+ */
+@Slf4j
+@Component
+public class LicenseCheckListener implements ApplicationListener<ContextRefreshedEvent> {
+
+//    private static Logger logger = LoggerFactory.getLogger(LicenseCheckListener.class);
+
+    /**
+     * 证书subject
+     */
+    @Value("${license.subject}")
+    private String subject;
+
+    /**
+     * 公钥别称
+     */
+    @Value("${license.publicAlias}")
+    private String publicAlias;
+
+    /**
+     * 访问公钥库的密码
+     */
+    @Value("${license.storePass}")
+    private String storePass;
+
+    /**
+     * 证书生成路径
+     */
+    @Value("${license.licensePath}")
+    private String licensePath;
+
+    /**
+     * 密钥库存储路径
+     */
+    @Value("${license.publicKeysStorePath}")
+    private String publicKeysStorePath;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        //root application context 没有parent
+        ApplicationContext context = event.getApplicationContext().getParent();
+        if (context == null) {
+            if (StringUtils.isNotBlank(licensePath)) {
+                log.info("++++++++ 开始安装证书 ++++++++");
+
+                LicenseVerifyParam param = new LicenseVerifyParam();
+                param.setSubject(subject);
+                param.setPublicAlias(publicAlias);
+                param.setStorePass(storePass);
+                param.setLicensePath(licensePath);
+                param.setPublicKeysStorePath(publicKeysStorePath);
+
+                LicenseVerify licenseVerify = new LicenseVerify();
+                //安装证书
+                licenseVerify.install(param);
+
+                log.info("++++++++ 证书安装结束 ++++++++");
+            }
+        }
+    }
+}
